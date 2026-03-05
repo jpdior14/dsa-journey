c177b57f3a0cfc4bc09f2b75fe7b0d411a0ee9ed
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-03-05 18:15:34 +0100

    Also this

---
+++ myprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> nums = {2, 7, 11, 15};
+
+	for (int i = 0; i < nums.size(); i++) {
+		cout << nums[i] << endl;
+	}
+	return 0;
+}
